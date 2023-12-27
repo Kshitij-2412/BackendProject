4a402bc0430aab3313866e33872c97c6004003f8
Kshitij Agarwal <kshitija024@gmail.com>
2023-12-27 15:48:03 +0530

    registered user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -15,16 +15,16 @@ const registerUser = asyncHandler(async (req, res) => {
   // check for user creation
   // return res
 
-  const { fullName, email, username, password } = req.body;
-  console.log("email : ", email);
+  const { fullname, email, username, password } = req.body;
+  // console.log("email : ", email);
 
   if (
-    [fullName, email, username, password].some((field) => field?.trim() === "")
+    [fullname, email, username, password].some((field) => field?.trim() === "")
   ) {
     throw new ApiError(400, "All fields are required");
   }
 
-  const existedUser = User.findOne({
+  const existedUser = await User.findOne({
     $or: [{ username }, { email }],
   });
 
@@ -33,13 +33,26 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 
   //this is provided by multer
-  const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  console.log("req.files:", req.files);
+  // const avatarLocalPath = req.files?.avatar[0]?.path;
+  const avatarLocalPath = req.files && req.files.avatar && req.files.avatar[0] && req.files.avatar[0].path;
+
+  // console.log("req.files:", req.files);
+  // console.log("avatarLocalPath:", avatarLocalPath);
+ 
+  // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  // const coverImageLocalPath = req.files && req.files.coverImage && req.files.coverImage[0] && req.files.coverImage[0].path;
+  let coverImageLocalPath;
+  if(req.files && Array.isArray(req.files.coverImage)&& req.files.coverImage.length > 0){
+    coverImageLocalPath=req.files.coverImage[0].path
+  }
+  
+  // console.log("avatarLocalPath:", avatarLocalPath);
 
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar file is required");
   }
-
+ 
   const avatar = await uploadOnCloudinary(avatarLocalPath);
   const coverImage = await uploadOnCloudinary(coverImageLocalPath);
 
@@ -48,7 +61,7 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 
   const user = await User.create({
-    fullName,
+    fullname,
     avatar: avatar.url,
     coverImage: coverImage?.url || "",
     email,
@@ -64,9 +77,9 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new ApiError(500, "Something went wrong when registering a user");
   }
 
-  return res.status(201).json(
-    new ApiResponse(200, createdUser, "User registered successfully")
-  )
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createdUser, "User registered successfully"));
 });
 
 export { registerUser };
